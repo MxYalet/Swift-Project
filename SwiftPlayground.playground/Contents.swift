53ea5718f1168908fa0ab64cc804a248aeca2fbc
mac <muhammedowoyale@gmail.com>
2024-02-01 23:39:46 +0100

    Initial Commit

---
+++ SwiftPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var array = [12,465,23,23]
+
+array.count
+ 
+array[3]
+
+//array.append(5)
+//array.insert(33, at: 3)
+//array.sort()
+//array.reverse()
+//array.shuffle()
+
+var arraySet: Set<Int> = []
+
+var Set = Set(array)
+print(array)
+ 
+

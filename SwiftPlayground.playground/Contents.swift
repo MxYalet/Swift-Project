8f37f618181f278606f3f0cda59717e6ca975a17
mac <muhammedowoyale@gmail.com>
2024-02-05 21:35:32 +0100

    func and dictionary

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -1,22 +1,48 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*var greeting = "Hello, playground"
 
 var array = [12,465,23,23]
 
 array.count
  
 array[3]
-
 //array.append(5)
 //array.insert(33, at: 3)
 //array.sort()
 //array.reverse()
 //array.shuffle()
+//var arraySet: Set<Int> = []
+var set = Set(array)
+print(array)*/
 
-var arraySet: Set<Int> = []
 
-var Set = Set(array)
+//Dictionary
+let devices: [String: String] = [
+    "Phone": "Iphone",
+    "Laptop": "M1 Macbook air",
+    "Pods" : "Oraimo freebuds 3"]
+devices["Pods"]
+
+//functions
+func addNumber(firstNumber: Int, to secondNumber: Int)-> Int{
+    let sum = firstNumber + secondNumber
+    return sum
+}
+addNumber(firstNumber: 23, to: 55)
+
+let allStars = ["james", "curry", "harden", "durant"]
+
+for players in allStars where players == "harden"{
+    print(players)
+}
+
+var array: [Int] = []
+
+for _ in 0..<25{
+    let randomBNumber = Int.random(in: 0...50)
+    array.append(randomBNumber)
+}
 print(array)
  
-
+ 

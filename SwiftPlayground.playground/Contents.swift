2c130cd0f4c6c1795c421ccaa932307ea2581203
mac <muhammedowoyale@gmail.com>
2024-02-12 22:37:21 +0100

    classes

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -45,4 +45,68 @@ for _ in 0..<25{
 }
 print(array)
  
+
+//enum
+enum phones: String{
+    case iphonex = "that was my previous phone",
+         Iphone12 = "this is my current phone",
+         samsungs24 = "samsung isn't bad also" ,
+         iphone15 =  "will get this when there's enough"
+}
+
+func getMuhammedOpinion(on phone: phones){
+  
+    print(phone.rawValue  )
+}
+getMuhammedOpinion(on: .iphone15)
  
+
+//Optionals
+
+var arr: [Int] = [21,5,25,98,45]
+arr.sort()
+
+
+    //if let
+if let oldestAge = arr.last{
+    print("the oldest is \(oldestAge)")
+}else{print("you must have no students")}
+
+    //nil coalescing
+let oldestAge = arr.last ?? 999 //giving it default value if null
+
+    //guard statement
+func getOldestAge(){
+    guard let oldestAge = arr.last else{
+        return
+    }
+    print("the oldest is \(oldestAge)")
+}
+
+
+class Developer{
+    
+    var name: String?
+    var jobTitle: String?
+    var yearOfExperience: Int?
+    
+    init() {
+          // This is the empty initializer
+      }
+    
+    init(name: String, jobTitle: String, yearOfExperience: Int) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.yearOfExperience = yearOfExperience
+    }
+    
+    func speakName()
+    {
+        print("I am " + name!)
+    }
+}
+
+let moh = Developer(name: "moh", jobTitle: "android dev", yearOfExperience: 4)
+
+moh.speakName()
+
